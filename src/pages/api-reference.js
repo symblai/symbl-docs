fa8f93e78c94a7717863277a7ecbb32d24fa7992
Kush Vora <vorakush@gmail.com>
2022-04-26 17:25:14 +0530

    integrate stoplight with docusaurus

---
+++ src/pages/api-reference.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Head from '@docusaurus/Head';
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
+import Layout from '@theme/Layout';
+
+export default function APIPage() {
+  return (
+    <Layout>
+      <Head>
+        <title>API Reference | Dyte Docs</title>
+        <meta name="description" content="Dyte REST API Reference" />
+        <meta name="og:description" content="Dyte REST API Reference" />
+        {/* Loading styles for elements this way so it doesn't interfere with other styles */}
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/@stoplight/elements@7.3.7/styles.min.css"
+        />
+      </Head>
+      <BrowserOnly
+        fallback={
+          <div className="flex min-h-screen w-full items-center justify-center">
+            <div
+              className="h-10 w-10 animate-spin rounded-full border-l border-t-2 border-primary"
+              aria-label="Loading..."
+            ></div>
+          </div>
+        }
+      >
+        {() => {
+          // eslint-disable-next-line no-undef
+          const { API } = require('@stoplight/elements');
+          return (
+            <>
+              <APIVersionSwitcher current="v1" />
+              <API
+                apiDescriptionUrl="/api/v1.yaml"
+                router="hash"
+                basePath="/"
+                layout="sidebar"
+                hideSchemas
+              />
+            </>
+          );
+        }}
+      </BrowserOnly>
+    </Layout>
+  );
+}
